19c4b6c168c237282242a4167ce04084327467cd
keyue-c <chenky_cc@outlook.com>
2020-03-30 17:29:04 +0100

    add stroop timeout (500msec) and a break

--- data_includes/main.js
+++ data_includes/main.js
@@ -2,7 +2,8 @@ PennController.ResetPrefix(null) // Shorten command names (keep this line here)
 
 // Show the 'intro' trial first, then all the 'experiment' trials in a random order
 // then send the results and finally show the trial labeled 'bye'
-Sequence( "intro", "practice_color", randomize("color_matching"), "practice_stroop", randomize("stroop"), "practice_combined", randomize("practice"), "exp_instru", randomize("experiment"), SendResults(), "bye" )
+Sequence( "intro", "practice_color", randomize("color_matching"), "end_color_matching", "practice_stroop", randomize("stroop"), 
+            "practice_combined", randomize("practice"), "exp_instru", randomize("experiment1"), "break", randomize("experiment2"), SendResults(), "bye" )
 
 
 // What is in Header happens at the beginning of every single trial
@@ -15,9 +16,16 @@ Header(
     newTimer(250)
         .start()
         .wait()
+    ,
+    newVar("score_matching",0)
+        .global()
+    ,
+    newVar("score_stroop",0)
+        .global()
 )
 .log( "ID" , getVar("ID") )
 // This log command adds a column reporting the participant's name to every line saved to the results
+
 
 // Part1 information and consent
 newTrial( "intro" ,
@@ -68,34 +76,54 @@ newTrial("practice_color",
 
 Template( "practice_matching_ibex.csv",
     row => newTrial("color_matching",
-                newText("col_pad", row.Word)
-                    .color(row.FontColourCode)
-                    .css("font-size", "7em")
-                    .print()
-                ,
-                newKey("re_col", "QWE")
-                    .wait()
-                ,
-                getText("col_pad")
-                    .remove()
-                ,
-                getKey("re_col")
-                    .test.pressed(row.Button)
-                        .success(newText("<br/><br/><br/><br/>CORRECT!")
-                            .print())
-                        .failure(newText("<br/><br/><br/><br/>INCORRECT!")
-                            .color("red")
-                            .print())
-                ,
-                newTimer(1000)
-                    .start()
-                    .wait()
-        )
+                getVar("score_matching")
+                    .test.is(6).failure(
+                                    newText("col_pad", row.Word)
+                                        .color(row.FontColourCode)
+                                        .css("font-size", "7em")
+                                        .print()
+                                    ,
+                                    newKey("re_col","QWE")
+                                    ,
+                                    newTimer(500)
+                                        .callback(
+                                            getText("col_pad").remove())
+                                        .start()
+                                        .wait()
+                                    ,
+                                    getKey("re_col")
+                                        .test.pressed(row.Button)
+                                            .success(newText("<br/><br/><br/><br/>CORRECT!")
+                                                .center()
+                                                .print(), 
+                                                getVar("score_matching").set(v=>v+1))
+                                            .failure(newText("<br/><br/><br/><br/>INCORRECT!")
+                                                .center()
+                                                .color("red")
+                                                .print())
+                                    ,
+                                    newText("display score", "")
+                                        .text(getVar("score_matching"))
+                                        .before(newText("left label", "You've got &nbsp;"))
+                                        .after(newText("right label", "&nbsp; correct"))
+                                        .print()
+                                    ,
+                                    newTimer(1000)
+                                        .start()
+                                        .wait()
+                                    )
+                )
 )
+
 
 newTrial("practice_stroop",
     newText("<p>Then we'll print colour names in different inks.</p><p>Press <b>space bar</b> to start practice</p>")
         .css("font-size", "1.2em")
+        .print()
+    ,
+    newText("<p><b>You must get 10 trails correct to proceed to the next step.</b></p>")
+        .css("font-size", "1.2em")
+        .color("red")
         .print()
     ,
     newKey(" ")
@@ -104,28 +132,42 @@ newTrial("practice_stroop",
 
 Template( "practice_stroop_ibex.csv",
     row => newTrial("stroop",
-                newText("word", row.Word)
-                    .color(row.FontColourCode)
-                    .css("font-size", "1.5em")
-                    .print()
-                ,
-                newKey("re_col", "QWE")
-                    .wait()
-                ,
-                getText("word")
-                    .remove()
-                ,
-                getKey("re_col")
-                    .test.pressed(row.Button)
-                        .success(newText("<br/><br/><br/>CORRECT!")
-                            .print())
-                        .failure(newText("<br/><br/><br/>INCORRECT!")
-                            .color("red")
-                            .print())
-                ,
-                newTimer(1000)
-                    .start()
-                    .wait()
+                getVar("score_stroop")
+                    .test.is(10).failure(
+                                    newText("word", row.Word)
+                                        .color(row.FontColourCode)
+                                        .css("font-size", "1.5em")
+                                        .print()
+                                    ,
+                                    newKey("re_col","QWE")
+                                    ,
+                                    newTimer(500)
+                                        .callback(
+                                            getText("word").remove())
+                                        .start()
+                                        .wait()
+                                    ,
+                                    getKey("re_col")
+                                        .test.pressed(row.Button)
+                                            .success(newText("<br/><br/><br/>CORRECT!")
+                                                .center()
+                                                .print(),
+                                                getVar("score_stroop").set(v=>v+1))
+                                            .failure(newText("<br/><br/><br/>INCORRECT!")
+                                                .center()
+                                                .color("red")
+                                                .print())
+                                    ,
+                                    newText("display score", "")
+                                        .text(getVar("score_stroop"))
+                                        .before(newText("left label", "You've got &nbsp;"))
+                                        .after(newText("right label", "&nbsp; correct"))
+                                        .print()
+                                    ,
+                                    newTimer(1000)
+                                        .start()
+                                        .wait()
+                                )
         )
 )
 
@@ -153,11 +195,14 @@ Template( "practice_combined_ibex.csv" ,
                     .css("font-size", "1.5em")
                     .print()
                 ,
-                newKey("re_prac","QWE")
-                    .wait()
+                newKey("QWE")
+                    .log()
                 ,
-                getText("Stroop")
-                    .remove()
+                newTimer(500)
+                    .callback(
+                        getText("Stroop").remove())
+                    .start()
+                    .wait()
                 ,
                 newController("DashedSentence",{s:row.Sentence})
                     .print()
@@ -186,10 +231,13 @@ Template( "practice_combined_ibex.csv" ,
                         .print()
                 ,
                 newKey("QWE")
-                    .wait()
+                    .log()
                 ,
-                getText("Stroop")
-                    .remove()
+                newTimer(500)
+                    .callback(
+                        getText("Stroop").remove())
+                    .start()
+                    .wait()
                 )
         )
 )
@@ -209,9 +257,9 @@ newTrial("exp_instru",
 )
 
 // This Template command generates as many trials as there are rows in stimuli_ibex.csv
-Template( "stimuli_ibex.csv" ,
+Template( "stimuli_ibex_test1.csv" ,
     // Row will iteratively point to every row in stimuli_ibex.csv
-    row => newTrial( "experiment" ,
+    row => newTrial( "experiment1" ,
         newText("TrialType", row.TrialType)
             .test.text("Stroop_Sentence")
             .success(
@@ -222,10 +270,12 @@ Template( "stimuli_ibex.csv" ,
                 ,
                 newKey("QWE")
                     .log()
-                    .wait()
                 ,
-                getText("Stroop")
-                    .remove()
+                newTimer(500)
+                    .callback(
+                        getText("Stroop").remove())
+                    .start()
+                    .wait()
                 ,
                 newController("DashedSentence",{s:row.Sentence})
                     .print()
@@ -259,14 +309,95 @@ Template( "stimuli_ibex.csv" ,
                 ,
                 newKey("QWE")
                     .log()
-                    .wait()
                 ,
-                getText("Stroop")
-                    .remove()
+                newTimer(500)
+                    .callback(
+                        getText("Stroop").remove())
+                    .start()
+                    .wait()
                 )
     )
-    .log("Group" , row.Group)
-    .log("Condition", row.Condition)
+.log("Group" , row.Group)
+.log("Condition", row.Condition)
+    // Add these three columns to the results lines of these Template-based trials
+)
+
+newTrial("break",
+    newText("<p>You can take a break or click the button to continue</p>")
+        .bold()
+        .css("font-size", "1.5em")
+        .print()
+    ,
+    newButton("Continue")
+        .center()
+        .css("font-size", "1.5em")
+        .print()
+        .wait()
+)
+
+// This Template command generates as many trials as there are rows in stimuli_ibex.csv
+Template( "stimuli_ibex_test2.csv" ,
+    // Row will iteratively point to every row in stimuli_ibex.csv
+    row => newTrial( "experiment2" ,
+        newText("TrialType", row.TrialType)
+            .test.text("Stroop_Sentence")
+            .success(
+                newText("Stroop", row.Word)
+                    .color(row.FontColourCode)
+                    .css("font-size", "1.5em")
+                    .print()
+                ,
+                newKey("QWE")
+                    .log()
+                ,
+                newTimer(500)
+                    .callback(
+                        getText("Stroop").remove())
+                    .start()
+                    .wait()
+                ,
+                newController("DashedSentence",{s:row.Sentence})
+                    .print()
+                    .log()
+                    .wait()
+                    .remove()
+                ,
+                newController("Question", {q:row.Question, as: ["yes","no"]})
+                    .print()
+                    .log()
+                    .wait()
+                    .remove()
+                )
+            .failure(
+                newController("DashedSentence",{s:row.Sentence})
+                    .print()
+                    .log()
+                    .wait()
+                    .remove()
+                ,
+                newController("Question", {q:row.Question, as: ["yes","no"]})
+                    .print()
+                    .log()
+                    .wait()
+                    .remove()
+                ,
+                newText("Stroop", row.Word)
+                        .color(row.FontColourCode)
+                        .css("font-size", "1.5em")
+                        .print()
+                ,
+                newKey("QWE")
+                    .log()
+                ,
+                newTimer(500)
+                    .callback(
+                        getText("Stroop").remove())
+                    .start()
+                    .wait()
+                )
+    )
+.log("Group" , row.Group)
+.log("Condition", row.Condition)
     // Add these three columns to the results lines of these Template-based trials
 )
 
